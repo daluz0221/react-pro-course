805195fbabedbb1ba0ece47b740c6665cd69ca00
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-08-07 14:15:18 -0500

    feat: terminado el patrón de control props

---
+++ src/02-components-patterns/data/products.ts
@@ -0,0 +1,18 @@
+import { Product } from "../interfaces/interfaces"
+
+const product = {
+    id: "1",
+    title: 'coffe-mug - Card',
+    img: './mug.png'
+}
+
+const product2 = {
+  id: "2",
+  title: 'coffe-mug - Dilema',
+  img: './mug2.png'
+}
+
+export const products: Product[] = [
+  product,
+  product2
+] 
